cedfd93322f149327e0928af8ee86726e61bc7bb
Rishabh32700 <rishabhpep@gmail.com>
2023-08-01 01:05:48 +0530

    heading added and filters done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
-const allCategories = ['all', ...new Set(items.map(item => item.category))];
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState(allCategories);
 
-  const filterItems = category => {
-    console.log('click', category);
-    if(category =='all'){
+  const filterItems = (category) => {
+    console.log("click", category);
+    if (category == "all") {
       setMenuItems(items);
       return;
     }
@@ -21,8 +21,12 @@ function App() {
   return (
     <main>
       <section className="menu section">
+        <div style={{ display: "flex", justifyContent: "center", flexDirection:"column", alignItems:"center" }}>
+          <h2>Fullstack Foods</h2>
+          <p style={{color:"var(--clr-gold)"}}>+91 7011671691</p>
+        </div>
         <div className="title">
-          <h2>our menu</h2>
+          <h3>our menu</h3>
           <div className="underline" />
         </div>
         <Categories categories={categories} filterItems={filterItems} />
